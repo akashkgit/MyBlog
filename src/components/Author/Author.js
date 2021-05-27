28a73fac97928911b2725885901c1d16dcaf0869
akash kumar <akashkvit@gmail.com>
2021-05-27 14:29:29 +0530

    scroll 1 + words

--- src/components/Author/Author.js
+++ src/components/Author/Author.js
@@ -6,8 +6,8 @@ const Author = () => (
 
   <div className={styles.author} id="auth" >
   <img className={styles.authorimg}src= {author} alt="error"/>
-   <div className={styles.authorwords} >" My Vision is to get   delighhted by learning multitude of technologies and leverage it to solve Business and
-   social cause by my soft skills. I like to Explore philosophy as well and I believe that inorder to suceed, one most be commited to what they want to achieve. A person with commitment is powerful
+   <div className={styles.authorwords} >" My Vision is to get   delighted by learning multitude of technologies and leverage it to solve Business and
+   social cause by using my soft skills.I believe that inorder to suceed, one most be commited to what they want to achieve. A person with commitment is powerful
     than a person with only interest.Start small and progress.Do Less,But better!"
   
   <div className={styles.name}>  -Akash kumar</div>
